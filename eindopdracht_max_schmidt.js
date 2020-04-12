5795c98473fb68d81eec212ee0348eff1cf8d4ce
Max Schmidt <max@grape.nl>
2020-04-12 13:45:06 +0200

    dit zijn mijn eindopcrachten van frontend

---
+++ eindopdracht_max_schmidt.js
@@ -0,0 +1,8 @@
+/*	Author: Max Schmidt
+	Date 12-4-2020
+	Name: eindopdracht_max_schmidt.js
+*/
+function myFunction() {
+  var inputVal = document.getElementById("superHero").value;
+  alert('Superhero ' + inputVal + ' has been registered!');
+}
